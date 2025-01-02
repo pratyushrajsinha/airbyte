7edb33e813f80a5c4fbc407086d42eb1ccdb5ba5
pnilan <patrick.nilan@airbyte.io>
2025-01-02 14:00:48 -0800

    update import statement and type ignore

--- airbyte-ci/connectors/pipelines/pipelines/helpers/utils.py
+++ airbyte-ci/connectors/pipelines/pipelines/helpers/utils.py
@@ -15,11 +15,11 @@ import sys
 import unicodedata
 import warnings
 import xml.sax.saxutils
+from exceptiongroup import ExceptionGroup
 from io import TextIOWrapper
 from pathlib import Path
 from typing import TYPE_CHECKING
 
-import anyio
 import asyncclick as click
 import asyncer
 from dagger import Client, Config, Container, Directory, ExecError, File, ImageLayerCompression, Platform, Secret
@@ -115,7 +115,7 @@ async def get_file_contents(container: Container, path: str) -> Optional[str]:
 def catch_exec_error_group() -> Generator:
     try:
         yield
-    except anyio.ExceptionGroup as eg:
+    except ExceptionGroup as eg:
         for e in eg.exceptions:
             if isinstance(e, ExecError):
                 raise e
