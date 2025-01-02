eab4dde8b33cc7130e4a519c5486a6b5644ecb51
pnilan <patrick.nilan@airbyte.io>
2025-01-02 14:06:56 -0800

    chore: format code

--- airbyte-ci/connectors/pipelines/pipelines/helpers/utils.py
+++ airbyte-ci/connectors/pipelines/pipelines/helpers/utils.py
@@ -15,7 +15,6 @@ import sys
 import unicodedata
 import warnings
 import xml.sax.saxutils
-from exceptiongroup import ExceptionGroup
 from io import TextIOWrapper
 from pathlib import Path
 from typing import TYPE_CHECKING
@@ -23,6 +22,7 @@ from typing import TYPE_CHECKING
 import asyncclick as click
 import asyncer
 from dagger import Client, Config, Container, Directory, ExecError, File, ImageLayerCompression, Platform, Secret
+from exceptiongroup import ExceptionGroup
 from more_itertools import chunked
 
 if TYPE_CHECKING:
